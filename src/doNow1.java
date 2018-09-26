287f81d164ed00cc6502c7f5af78d209eabbfa7e
BT_1N3_28@BKTECH.LOCAL <BT_1N3_28@BKTECH.LOCAL>
2018-09-26 09:18:30 -0400

    Initial commit

---
+++ src/doNow1.java
@@ -0,0 +1,12 @@
+public class doNow1 {
+
+    public static void main(String[] args)
+    {
+        String str1 = "LoveJava";
+        while(str1.length() > 0)
+        {
+            System.out.println(str1);
+            str1 = str1.substring(1, str1.length() - 1);
+        }
+    }
+}
